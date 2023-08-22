54ff89845bee00f9e12c169a010ca589fd6ad5e7
qLoki <bobayan2005@gmail.com>
2023-08-22 11:55:02 +0300

    initial commit

---
+++ EFLesson1Training/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EFLesson1Training
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ Lesson1(EFCore_DBLibrary)/Lesson1(EFCore_DBLibrary)/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Lesson1_EFCore_DBLibrary_
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext()
+        {
+                
+        }
+        public ApplicationDbContext( DbContextOptions options) : base(options)
+        {
+                            
+        }
+    }
+}
