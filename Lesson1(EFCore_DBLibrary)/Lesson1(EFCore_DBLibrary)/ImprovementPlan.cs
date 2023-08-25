75741ee869ee75970a2b389d3786231a87c3f2bc
qLoki <bobayan2005@gmail.com>
2023-08-25 16:14:26 +0300

    End of the first-code approach with existing project
    bye bye project(

---
+++ Lesson1(EFCore_DBLibrary)/Lesson1(EFCore_DBLibrary)/ImprovementPlan.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lesson1_EFCore_DBLibrary_
+{
+	public class ImprovementPlan
+	{
+		[Key]
+		[ForeignKey("Employee")]
+		public int BusinessEntityId { get; set; }
+		public virtual Employee Employee { get; set; }
+		[Required]
+		public DateTime PlanStart { get; set; }
+		public DateTime PlanComplete => PlanStart.AddDays(90);
+	}
+}
---
+++ Lesson1(EFCore_DBLibrary)/Lesson1(EFCore_DBLibrary)/Migrations/20230824140804_Initial_migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lesson1_EFCore_DBLibrary_.Migrations
+{
+	/// <inheritdoc />
+	public partial class Initial_migration : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			//kniger
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			//niger
+		}
+	}
+}
